49a6d607ccbe33f821f6c6a388f2958ba3107db6
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-10 21:21:48 +0530

    Add files via upload

---
+++ python_list.py
@@ -0,0 +1,76 @@
+
+#numbers = [0,1,2,3,4,5,6,7,8,9,10,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+#print(numbers)
+
+
+#numbers = [1,2,3,4,5,-1,-2,-3,-4,-5]
+#name = ['priya','monika','priyanshi','manshi','ravi']
+#mix_list = [1,'priya',-10,66.4,'true']
+#print(numbers)
+#print(name)
+#print(mix_list)
+#print(len(numbers))
+#print(len(name))
+#print(len(mix_list))
+
+
+#numbers = [1,2,3,-1,-2,-3]
+#print(numbers[0:1])
+#print(numbers[2:4])
+#print(numbers[3:])
+#print(numbers[:])
+#print(numbers[::-1])
+#print(numbers[0:5:1])
+
+
+#number = [1,2,3,4,5]
+#number.sort()
+#print(number)
+
+
+#number = [1,2,3,4,5]
+#number.reverse()
+#print(number)
+
+
+#number = [1,2,3,4,5]
+#print(min(number))
+#print(max(number))
+
+
+#number = [1,2,3,4,5]
+#number.append(6)
+#number.append(7)
+#print(number)
+
+#number = [1,2,3,4,5]
+#number.remove(5)
+#number.remove(4)
+#print(number)
+
+#numbers = [1,2,5]
+#numbers.insert(2,3)
+#numbers.insert(3,4)
+#print(numbers)
+
+
+#number = [1,2,3,4,5]
+#number.extend([6,7,8,9])
+#print(number)
+
+
+#number = [1,2,3,4,5,6,7,8,9]
+#number.remove(6)
+#number.remove(7)
+#number.remove(8)
+#number.remove(9)
+#print(number)
+
+
+#number = [1,2,3,4,5,6,7,8,9]
+#number.pop(8)
+#print(number)
+
+
+number = [1,2,3,4,5,6,7,8,9]
+print(number.count(2))
