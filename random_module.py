6f4f8dda33e9e9c9cf2ee3e1be15c087dae079fc
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-11 14:32:04 +0530

    Add files via upload

---
+++ random_module.py
@@ -0,0 +1,10 @@
+import random
+
+#a=random.randint(1,3)
+#a=random.randrange(1,3)
+#a=random.random()
+#a=random.uniform(1,3)
+l=[5,3,84,-8,13,65]
+#a=random.choice(l)
+#random.shuffle(l)
+print(l)
