9b659175ee5088b18a9142145f589e9a4639b1c7
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-12 13:37:31 +0530

    Add files via upload

--- index_error.py
+++ index_error.py
@@ -2,5 +2,5 @@ name=['priyanshi','monika','ravi','bimlesh','suresh']
 length=len(name)
 #print(f"Hi,{name[2]}")
 #print(f"Hi,{name[4]}")
-#print(f"Hi,{name[-5]}")
-print(f"Hi,{name[6]}")
+print(f"Hi,{name[length-1]}")
+#print(f"Hi,{name[6]}")
