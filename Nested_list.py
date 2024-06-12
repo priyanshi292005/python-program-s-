d30aa9297526cad1d7019877e57bd4d8c4a743e2
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-12 14:11:19 +0530

    Add files via upload

---
+++ Nested_list.py
@@ -0,0 +1,14 @@
+#list = [1,2,3,4,5,[10,11,12],7,8,9]
+#print(list)
+#print(len(list))
+#print(list[0])
+#print(list[5][2])
+#print(list[len(list)-1])
+#print(list[5][:])
+#print(list[5][:0])
+#print(list[5][:3])
+
+
+list = [10,34,90,['mohan','shyam','ram'],89]
+#print(list)
+print(list[3][2])
