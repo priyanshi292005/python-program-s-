317b7c8274b71166a6f7f6f2b1e9bfb72881f840
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-13 14:55:44 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,4 @@
+tuple1 = (1, 2, 3, 'priya' , 'monika')
+print('Tuples == Tuples use to store multiples itemsin single variable ')
+print(tuple1)
+print(tuple1[0], tuple1[1], tuple1[2] , tuple1[3], tuple1[4])
