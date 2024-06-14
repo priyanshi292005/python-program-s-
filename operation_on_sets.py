d3867a99eb84bb64e43aa65d705296b30b3cca3d
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-14 13:47:47 +0530

    Add files via upload

---
+++ operation_on_sets.py
@@ -0,0 +1,22 @@
+
+set1 = {'priya', 'monika', 'ravi'}
+set2 = {'priya', 'mansi', 'hitanshi'}
+#print(set1.intersection(set2))
+#print(set1.union(set2))
+#print(set1.symmetric_difference(set2))
+#print(set1.difference(set2))
+#print(set2.difference(set1))
+#print(set2.symmetric_difference(set1))
+#print(set1 - set2)
+#print(set2 - set1)
+#print(set1 & set2)
+#set1.difference_update(set2)
+#print(set1)
+#print(set2)
+#print(set1 ^ set2)
+#print(set1.isdisjoint(set2))
+#print(set1.issubset(set2))
+#print(set1.issuperset(set2))
+#print(set1 <= set2)
+#print(set2 >= set1)
+
