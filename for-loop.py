a58ce8cda46af426737a98f94d8629b9736e21ec
priyanshi292005 <priyanshichauhan292005@gmail.com>
2024-06-14 14:20:06 +0530

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,14 @@
+#name = ['priya','hitanshi','monika']
+#for i in name:
+#    print(i)
+#    if i=='priya':
+#        print("hey, it's me")
+
+
+
+numbers = [1, 4, -1, -3, 10]
+squares =[]
+for i in numbers:
+    square = i ** 2
+    squares.append(square)
+print(squares)
