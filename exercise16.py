61da6a20de13fc5a7fe0cf226a878eab69c4b61b
Priyanshi <priyanshichauhan292005@gmail.com>
2024-06-16 13:12:59 +0530

    Add files via upload

---
+++ exercise16.py
@@ -0,0 +1,15 @@
+
+#total=0
+#for i in range(2,101,2):
+#    total+=i
+#print(total)
+
+
+totl=0
+for i in range(2,101,2):
+    if i%2==0:
+        totl+=i
+        print(totl)
+
+
+print('these two type to find sum of even numbers from 1 to 100..')
