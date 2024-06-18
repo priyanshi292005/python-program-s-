3093a72a1d2bd0c1cf81b616c179bcd53429a35b
Priyanshi <priyanshichauhan292005@gmail.com>
2024-06-18 15:47:49 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,42 @@
+#count=1
+#while count<=5:
+#    print(count)
+#   count = count+1
+#print("out from loop")
+
+
+#count = 5
+#while count >=1:
+#    print(count)
+#    count -= 1
+
+
+#count = 5
+#while count >=1:
+#    print(count)
+#    count -= 1
+#else:
+#     print("in else block")
+#print("out from loop")
+
+
+#count = 5
+#while count >=1:
+#    print(count)
+#    count -= 1
+#    if count == 3:
+#        break
+#else:
+ #    print("in else block")
+#print("out from loop")
+
+
+number=int(input("Enter a number(-1 to quite): "))
+while number !=-1:
+    print(number)
+    number = int(input("Enter a number(-1 to quite): "))
+else:
+     print("in else block")
+print("out from loop")
+
+
