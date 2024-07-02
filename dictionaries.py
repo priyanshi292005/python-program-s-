b17297a22872763430fb29e1745af5749dd77750
Priyanshi <priyanshichauhan292005@gmail.com>
2024-07-02 12:30:25 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,85 @@
+print("EXAMPLE==1")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number)
+
+
+print("EXAMPLE==2")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number['shyam'])
+
+
+print("EXAMPLE==3")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+    'ram': 6666
+}
+print(phone_number)
+
+
+print("EXAMPLE==4")
+phone_number=dict({
+    'ram': 53279,
+    'shyam': 66453,
+})
+print(phone_number)
+
+
+print("EXAMPLE==5")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+del phone_number['ram']
+print(phone_number)
+
+
+print("EXAMPLE==6")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number.popitem())
+print(phone_number)
+
+
+print("EXAMPLE==7")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number.clear())
+print(phone_number)
+
+
+print("EXAMPLE==8")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number.keys())
+print(phone_number)
+
+
+print("EXAMPLE==9")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number.values())
+print(phone_number)
+
+
+print("EXAMPLE==10")
+phone_number = {
+    'ram': 53279,
+    'shyam': 66453,
+}
+print(phone_number.items())
+print(phone_number)
