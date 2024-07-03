959926433fe02b01239869a38e83fef5ef531e36
Priyanshi <priyanshichauhan292005@gmail.com>
2024-07-03 13:08:44 +0530

    Add files via upload

---
+++ function_with_return_value.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    c=a+b
+    return c
+
+print(add(5,4))
+
+
+def add(a,b):
+    c=a+b
+    return c
+
+result=add(5,4)
+print(result)
+
+
+def add(a,b):
+    c=a+b
+    return
+
+result=add(5,4)
+print(result)
