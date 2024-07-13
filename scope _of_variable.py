c9a69513e884eb7ee4e211cde9f2d7268b89be11
Priyanshi <priyanshichauhan292005@gmail.com>
2024-07-13 16:33:21 +0530

    Add files via upload

---
+++ scope _of_variable.py
@@ -0,0 +1,21 @@
+a=10
+def display():
+    a=15
+    print(a)
+display()
+
+
+
+a=10
+def display():
+    a=39
+    def show():
+        print(a)
+    show()
+display()
+
+
+a,b=5,6
+if a<b:
+    c=a+b
+    print(c)
