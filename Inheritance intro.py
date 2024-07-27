e74ebc82d93cec5d6d9927c0c2dec46e8708b690
Priyanshi <priyanshichauhan292005@gmail.com>
2024-07-27 11:30:37 +0530

    Add files via upload

---
+++ Inheritance intro.py
@@ -0,0 +1,12 @@
+class Human:
+    def work(self):
+        print("I can work")
+    def eat(self):
+        print("I can eat")
+class Male:
+    def eat(self):
+        print("I can eat")
+    def work(self):
+        print("I can work")
+Male_1=Male()
+Male_1.eat()
